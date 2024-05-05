58701cd7bd950069e2e68bd2e5278a3879f7f842
Maylebuh <malebomawela96@gmail.com>
2024-05-05 09:43:57 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,32 @@
 #!/usr/bin/python3
-""" a Fabric script (based on the file 1-pack_web_static.py) that distributes..
-    ..an archive to your web servers, using the function do_deploy: """
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
-
-from fabric.api import *
-from datetime import datetime
+from fabric.api import put, run, env
 from os.path import exists
-
-
-env.hosts = ['35.237.166.125', '54.167.61.201']  # <IP web-01>, <IP web-02>
-# ^ All remote commands must be executed on your both web servers
-# (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
+env.hosts = ['34.239.254.111', '35.153.66.238']
 
 
 def do_deploy(archive_path):
-    """ distributes an archive to my web servers
-    """
-    if exists(archive_path) is False:
-        return False  # Returns False if the file at archive_path doesnt exist
-    filename = archive_path.split('/')[-1]
-    # so now filename is <web_static_2021041409349.tgz>
-    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
-    # curr = '/data/web_static/current'
-    tmp = "/tmp/" + filename
+    """Distribute an archive to the web servers."""
+    if not exists(archive_path):
+        return False
 
     try:
-        put(archive_path, "/tmp/")
-        # ^ Upload the archive to the /tmp/ directory of the web server
-        run("mkdir -p {}/".format(no_tgz))
-        # Uncompress the archive to the folder /data/web_static/releases/
-        # <archive filename without extension> on the web server
-        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
-        run("rm {}".format(tmp))
-        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
-        run("rm -rf {}/web_static".format(no_tgz))
-        # ^ Delete the archive from the web server
-        run("rm -rf /data/web_static/current")
-        # Delete the symbolic link /data/web_static/current from the web server
-        run("ln -s {}/ /data/web_static/current".format(no_tgz))
-        # Create a new the symbolic link /data/web_static/current on the
-        # web server, linked to the new version of your code
-        # (/data/web_static/releases/<archive filename without extension>)
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run(f'mkdir -p {path}{no_ext}/')
+        run(f'tar -xzf /tmp/{file_name} -C {path}{no_ext}/')
+        run(f'rm /tmp/{file_name}')
+        run(f'mv {path}{no_ext}/web_static/* {path}{no_ext}/')
+        run(f'rm -rf {path}{no_ext}/web_static')
+        run(f'rm -rf /data/web_static/current')
+        run(f'ln -s {path}{no_ext}/ /data/web_static/current')
         return True
-    except:
+    except FileNotFoundError:
+        print("File not found.")
         return False
